d416645fac03f10c8722bbe8ed56e905166e4246
Brahm Singh <brahm.singh@ibm.com>
2024-04-01 13:53:28 +0530

    index file updated
    new index html

---
+++ assets/js/app.js
@@ -0,0 +1,90 @@
+
+$("input[name=rdYes]").change(function () {
+    if(this.value == "Yes")
+    {
+        $("#processImprovement").css('display', 'block');
+    }
+    else
+    {
+        $("#processImprovement").css('display', 'none');
+    }
+});
+
+$("input[name=rd1Yes]").change(function () {
+    if(this.value == "Yes")
+    {
+        $("#processImprovement_1").css('display', 'block');
+    }
+    else
+    {
+        $("#processImprovement_1").css('display', 'none');
+    }
+});
+
+$("input[name=rd2Yes]").change(function () {
+    if(this.value == "Yes")
+    {
+        $("#processImprovement_2").css('display', 'block');
+    }
+    else
+    {
+        $("#processImprovement_2").css('display', 'none');
+    }
+});
+
+$("input[name=rd3Yes]").change(function () {
+    if(this.value == "Yes")
+    {
+        $("#processImprovement_3").css('display', 'block');
+    }
+    else
+    {
+        $("#processImprovement_3").css('display', 'none');
+    }
+});
+
+$("input[name=rd4Yes]").change(function () {
+    if(this.value == "Yes")
+    {
+        $("#processImprovement_4").css('display', 'block');
+    }
+    else
+    {
+        $("#processImprovement_4").css('display', 'none');
+    }
+});
+
+$("input[name=rd5Yes]").change(function () {
+    if(this.value == "Yes")
+    {
+        $("#processImprovement_5").css('display', 'block');
+    }
+    else
+    {
+        $("#processImprovement_5").css('display', 'none');
+    }
+});
+
+$("input[name=rd6Yes]").change(function () {
+    if(this.value == "Yes")
+    {
+        $("#processImprovement_6").css('display', 'block');
+    }
+    else
+    {
+        $("#processImprovement_6").css('display', 'none');
+    }
+});
+
+$("input[name=rd3Yes]").change(function () {
+    if(this.value == "Yes")
+    {
+        $("#processImprovement_3").css('display', 'block');
+    }
+    else
+    {
+        $("#processImprovement_3").css('display', 'none');
+    }
+});
+
+
